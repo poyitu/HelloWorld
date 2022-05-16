023d76a5b3911c15b18e59f5bc59c2e192e84b0c
poyitu <2430820793@qq.com>
2022-05-16 13:14:06 +0800

    修改代理仓库

--- app/src/main/java/com/example/helloworld/listview/ListViewActivity.java
+++ app/src/main/java/com/example/helloworld/listview/ListViewActivity.java
@@ -2,16 +2,20 @@ package com.example.helloworld.listview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
 import com.example.helloworld.R;
 
 public class ListViewActivity extends Activity {
+    private ListView mLv1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
+        mLv1 = findViewById(R.id.lv_1);
+        mLv1.setAdapter(new MyListAdapter(ListViewActivity.this));
     }
 }
--- app/src/main/java/com/example/helloworld/listview/MyListAdapter.java
+++ app/src/main/java/com/example/helloworld/listview/MyListAdapter.java
@@ -5,6 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.helloworld.R;
 
 /**
  * @author Administrator
@@ -33,8 +38,31 @@ public class MyListAdapter extends BaseAdapter {
         return 0;
     }
 
+    static class ViewHolder {
+        public ImageView imageView;
+        public TextView tvTitle, tvTime, tvContent;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return  ;
+        ViewHolder holder = null;
+        if (convertView == null) {
+            convertView = mlayoutInflater.inflate(R.layout.layout_list_item, null);
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.iv_1);
+            holder.tvTitle = convertView.findViewById(R.id.tv_title);
+            holder.tvTime = convertView.findViewById(androidx.core.R.id.time);
+            holder.tvContent = convertView.findViewById(R.id.tv_content);
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.tvContent.setText("这是内容");
+        holder.tvTime.setText("2020-08-07");
+        holder.tvTitle.setText("这是标题");
+        Glide.with(mcontext).load("https://www.baidu.com/img/flexible/logo/pc/result.png").into(holder.imageView);
+
+        return convertView;
     }
 }
