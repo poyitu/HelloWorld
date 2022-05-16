be3a4158e2101ed06cf452ba692c9c46080537d3
poyitu <2430820793@qq.com>
2022-05-16 14:10:05 +0800

    设置了点击和长按事件

--- app/src/main/java/com/example/helloworld/listview/ListViewActivity.java
+++ app/src/main/java/com/example/helloworld/listview/ListViewActivity.java
@@ -2,7 +2,10 @@ package com.example.helloworld.listview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -17,5 +20,19 @@ public class ListViewActivity extends Activity {
         setContentView(R.layout.activity_listview);
         mLv1 = findViewById(R.id.lv_1);
         mLv1.setAdapter(new MyListAdapter(ListViewActivity.this));
+        mLv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this, "点击 pos", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mLv1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListViewActivity.this, "长按 pos", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
